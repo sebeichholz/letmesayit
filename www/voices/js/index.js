7043e364b6dfb83efe454c2bc0161055f1c98a8d
Sebastian Eichholz <gene@oregami.org>
2017-01-13 22:14:55 +0100

    more features for voice detection

--- www/voices/js/index.js
+++ www/voices/js/index.js
@@ -6,12 +6,21 @@ function populateVoiceList() {
   voices = speechSynthesis.getVoices();
 
   for(var i = 0; i < voices.length ; i++) {
-    var option = i + ') ';
+    var option = '';
     option += voices[i].name + ' (' + voices[i].lang + ')';
     
     option += ', lang=' + voices[i].lang;
     option += ', name=' +  voices[i].name;
-    document.getElementById("voices").append(option + "\n");
+    document.getElementById('voices').innerHTML += (option + "<br/>");
+
+    if (voices[i].lang.indexOf('de') != -1) {
+        document.getElementById('voices_de').innerHTML += (option + "<br/>");
+    }
+
+      if (voices[i].lang.indexOf('en') != -1) {
+          document.getElementById('voices_en').innerHTML += (option + "<br/>");
+      }
+
   }
     document.getElementById("voiceCount").innerHTML = voices.length + ' Stimmen';
     document.getElementById("userAgent").innerHTML = navigator.userAgent;
