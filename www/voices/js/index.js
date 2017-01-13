799907b07e0a934aee38f01658846ecf81e15795
Sebastian Eichholz <gene@oregami.org>
2017-01-13 19:02:26 +0100

    voices debug site

---
+++ www/voices/js/index.js
@@ -0,0 +1,28 @@
+function populateVoiceList() {
+  if(typeof speechSynthesis === 'undefined') {
+    return;
+  }
+
+  voices = speechSynthesis.getVoices();
+
+  for(i = 0; i < voices.length ; i++) {
+    var option = i + ') ';
+    option += voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option += ' -- DEFAULT';
+    }
+
+    option += ', lang=' + voices[i].lang;
+    option += ', name=' +  voices[i].name;
+    document.getElementById("voices").append(option + "\n");
+  }
+    document.getElementById("voiceCount").append(voices.length + ' Stimmen');
+    document.getElementById("userAgent").append(navigator.userAgent);
+
+}
+
+populateVoiceList();
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
