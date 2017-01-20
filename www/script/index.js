4755e7e425d1f9c972cba94e76175ed86b5fb2f4
Sebastian Eichholz <gene@oregami.org>
2017-01-20 22:33:04 +0100

    migration to VueJS

--- www/script/index.js
+++ www/script/index.js
@@ -1,156 +1,188 @@
-var localVoice;
-var localVoices = new Array();
-
-var localLanguage = 'de';
-
-var count = 0;
-var init = 0;
-
+/*
 function populateVoiceList() {
+
     if(typeof speechSynthesis === 'undefined') {
         return;
     }
 
-    if (init == 1 || localVoices.length > 0) {
-        console.log("already done");
-        return;
-    }
-
-    var voices = speechSynthesis.getVoices();
-
-    count++;
-    console.log('count: ' + count);
-
-    init = 1;
     for(var i = 0; i < voices.length ; i++) {
-        var v = voices[i];
-        localVoices.push(v);
+        var option = '';
+        option += voices[i].name + ' (' + voices[i].lang + ')';
 
-        var option = document.createElement('option');
-        option.textContent = v.name + ' (' + v.lang + ')';
+        option += ', lang=' + voices[i].lang;
+        option += ', name=' +  voices[i].name;
+        document.getElementById('voices').innerHTML += (option + "<br/>");
 
-        option.setAttribute('data-lang', v.lang);
-        option.setAttribute('data-name', v.name);
-        document.getElementById("voiceList").appendChild(option);
+        if (voices[i].lang.indexOf('de') != -1) {
+            document.getElementById('voices_de').innerHTML += (option + "<br/>");
+        }
+
+        if (voices[i].lang.indexOf('en') != -1) {
+            document.getElementById('voices_en').innerHTML += (option + "<br/>");
+        }
+
     }
-
-    console.log(voices.length + ' voices added.');
-
-    if (localVoices && localVoices.length>0) {
-        document.getElementById('voiceListContainer').style.display='';
-    }
-    else {
-        document.getElementById('voiceListContainer').style.display='none';
-    }
+    document.getElementById("voiceCount").innerHTML = voices.length + ' Stimmen';
+    document.getElementById("userAgent").innerHTML = navigator.userAgent;
 
 }
 
-
+stateChange();
 if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
-function sayIt(text) {
-    if (text.length == 0) {
-        editAction();
-        return;
-    }
+*/
 
-    var utterance = new SpeechSynthesisUtterance(text);
+new Vue({
+    el: '#app',
+    data: {
+        sayThis: '',
+        buttonsDisabled: false,
+        activeVoiceName: null,
+        localVoices: new Array(),
+        count: 0,
+        init: 0,
+        selectedVoice: null,
+        settingsVisible: false,
+        xxx: null,
+    },
+    methods: {
+        talk: function () {
+            var self = this;
 
-    if (localVoice) {
-        utterance.voice = localVoice;
-    }
+            if (this.sayThis.length == 0) {
+                this.edit();
+                return;
+            }
+
+            var utterance = new SpeechSynthesisUtterance(this.sayThis);
+
+            utterance.addEventListener('start', function () {
+                self.buttonsDisabled = true;
+
+            });
+
+            utterance.addEventListener('end', function () {
+                self.buttonsDisabled = false;
+                self.edit();
+            });
+
+            utterance.voice = null;
+            if (this.selectedVoice && typeof this.selectedVoice != 'undefined') {
+                //console.log('this.selectedVoice = --' + this.selectedVoice + '--');
+                var v = null;
+                for (var i=0; i < this.localVoices.length; i++) {
+                    if (this.localVoices[i].name === this.selectedVoice) {
+                        v = this.localVoices[i];
+                    }
+                }
+                utterance.voice = v;
+            }
+
+            this.log('say' + ((utterance.voice && typeof utterance.voice != 'undefined') ? ('&voice=' + utterance.voice.name + '(' + utterance.voice.lang + ')') : ''));
+            window.speechSynthesis.speak(utterance);
+
+        },
+
+        edit: function () {
+            var input = document.getElementById('sayThis');
+            var len = input.value.length * 2;
+            input.setSelectionRange(len, len);
+            setTimeout(function () {
+                input.focus();
+            }, 5);
+        },
+
+        clearInput: function () {
+            this.sayThis = '';
+            this.edit();
+        },
+
+        log: function (action) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'do.html?action=' + action);
+            xhr.send();
+        },
+
+        checkSpeechsynthesis: function () {
+            if (!Modernizr.speechsynthesis) {
+                window.location = "noSpeech.html";
+            }
+        },
+
+        populateVoiceList: function () {
+            var self = this;
+            if (typeof speechSynthesis === 'undefined') {
+                return;
+            }
+
+            if (this.init == 1 || this.localVoices.length > 0) {
+                console.log("already done");
+                return;
+            }
+
+            var voices = speechSynthesis.getVoices();
+
+            setTimeout(function () {
+                self.populateVoiceListNew(voices)
+            }, 500);
 
 
-    utterance.addEventListener('start', function () {
-        /*
-         input.disabled=true;
-         buttonSayIt.disabled=true;
-         buttonEdit.disabled=true;
-         buttonClear.disabled=true;
-         */
-    });
-    utterance.addEventListener('end', function () {
-        /*
-         input.disabled=false;
-         buttonSayIt.disabled=false;
-         buttonEdit.disabled=false;
-         buttonClear.disabled=false;
-         */
-        editAction();
-    });
 
-    if (localLanguage) {
-        utterance.lang = localLanguage;
-    } else {
-        utterance.lang = 'de';
-    }
+        },
+        populateVoiceListNew: function (voices) {
+            this.count++;
+            console.log('count: ' + this.count);
 
-    utterance.pitch = 1.0;
-    utterance.rate = 1;
-    window.speechSynthesis.speak(utterance);
-    log('say' + ((localVoice && typeof localVoice != 'undefined') ? ('&voice=' + localVoice.name + '(' + localVoice.lang + ')') : ''));
-}
+            this.init = 1;
+            for (var i = 0; i < voices.length; i++) {
+                var v = voices[i];
+                this.localVoices.push(v);
 
+                var option = document.createElement('option');
+                option.textContent = v.name + ' (' + v.lang + ')';
 
-function clearInput() {
-    document.getElementById('sayThis').value = '';
-    document.getElementById('sayThis').focus();
-    log('clear');
-}
+                option.setAttribute('data-lang', v.lang);
+                option.setAttribute('data-name', v.name);
+                //document.getElementById("voiceList").appendChild(option);
+            }
 
-function edit() {
-    log('edit');
-    editAction();
-}
+            console.log(voices.length + ' voices added.');
 
-function editAction() {
-    var input = document.getElementById('sayThis');
-    input.focus();
-    var len = input.value.length * 2;
-    input.setSelectionRange(len, len);
-}
+            if (this.localVoices && this.localVoices.length > 0) {
+                document.getElementById('voiceListContainer').style.display = '';
+            }
+            else {
+                document.getElementById('voiceListContainer').style.display = 'none';
+            }
+        },
+        selectVoice: function () {
+            alert('selectVoice');
+        },
 
-function log(action) {
-    $.ajax({
-        url: 'do.html?action=' + action,
-        success: function (data) {
-            // Do nothing
+        settings: function () {
+            this.settingsVisible = !this.settingsVisible;
         }
-    });
-}
 
+    },
+    mounted()
+    {
+        this.checkSpeechsynthesis();
+        if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+            speechSynthesis.onvoiceschanged = this.populateVoiceList;
+        };
 
+        document.getElementById("sayThis").focus();
 
-function updateVoice(select) {
-    var myoption = select.options[select.selectedIndex];
-    console.log(myoption);
-    var lang = myoption.dataset.lang;
-    var name = myoption.dataset.name;
+        this.populateVoiceList();
 
-    console.log('name = ' + name);
-    if (name) {
-        localVoice = localVoices.filter(function (voice) {
-            return voice.name == name;
-        })[0];
-    } else {
-        localVoice = null;
+    },
+
+    computed: {
+
     }
-}
-
-
-window.onload = function () {
-    checkSpeechsynthesis();
-    document.getElementById("sayThis").focus();
-    populateVoiceList();
-};
-
-function checkSpeechsynthesis() {
-    if (!Modernizr.speechsynthesis) {
-        window.location = "noSpeech.html";
-    }
-}
+})
 
 
 
