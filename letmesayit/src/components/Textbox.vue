2b3bc9e17e7f76906ba10596ac4b4e59380e19d9
Sebastian Eichholz <gene@kultpower.de>
2017-02-09 22:33:36 +0100

    fix german translation

--- letmesayit/src/components/Textbox.vue
+++ letmesayit/src/components/Textbox.vue
@@ -30,7 +30,7 @@
       <form id="settings" v-show="settingsVisible">
         <div class="row">
           <div class="medium-6 columns" id="voiceListContainer" v-show="localVoices.length>0" style="display: none;">
-            <label><b>{{ $t('voice') }}</b>
+            <label><b>{{ $t('voice') }}:</b>
               <!--<input style="margin: 0; margin-left: 1cm;" id="filter_voices" value="de" type="checkbox"/> nur deutsche Stimmen-->
               <select v-model="selectedVoice" id="selectVoice">
                 <!--<option value="null">Standard</option>-->
--- letmesayit/src/locales/de.js
+++ letmesayit/src/locales/de.js
@@ -1,4 +1,4 @@
 export default {
   "typeHere" : "hier schreiben",
-  "voice" : "Stimme_deutsch"
+  "voice" : "Stimme"
 }
